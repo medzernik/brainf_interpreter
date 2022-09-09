4da6fd8d3897db490697504906da49bf355a2fed
medzernik <medzernik1@gmail.com>
2022-09-09 18:29:12 +0200

    splitting in progress

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use std::fs::File;
 use std::io::BufReader;
 use std::io::prelude::*;
+use std::string;
+use std::string::String;
 
 use clap::Parser;
+use regex::Regex;
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
@@ -16,19 +19,27 @@ struct Args {
 fn main() {
     let args = Args::parse();
 
-    let file_contents_char = load_file(args.file);
+    let file_contents_char = load_file(&args.file);
+
+    triage_commands(file_contents_char);
+
+    let file_contents_string = load_file_string(&args.file);
+
+    let output_string_contents = generate_by_brackets(&file_contents_string);
+
+    println!("{}", output_string_contents);
 }
 
 /// This function loads the file and populates an array of chars. Then it is returned and can be processed further.
 /// usage:
 /// ```let _ = loadfile("examples\01-hello-world.bf");```
-fn load_file(file_name: String) -> Vec<char> {
+fn load_file(file_name: &String) -> Vec<char> {
     let mut file_contents_char: Vec<char> = Vec::new();
 
     let file = File::open(file_name).expect("error opening the file");
     let reader = BufReader::new(file);
 
-    //
+    //old function
     for line in reader.lines() {
         for ch in line.expect("Unable to read line").chars() {
             file_contents_char.push(ch);
@@ -36,4 +47,60 @@ fn load_file(file_name: String) -> Vec<char> {
     }
 
     return file_contents_char;
+}
+
+///Load the file into a string (one lined)
+fn load_file_string(file_name: &String) -> String {
+    let mut file_contents_char = String::new();
+
+    let file = File::open(file_name).expect("error opening the file");
+    let reader = BufReader::new(file);
+
+    //old function
+    for line in reader.lines() {
+        for ch in line.expect("Unable to read line").chars() {
+            file_contents_char.push(ch);
+        }
+    }
+
+    return file_contents_char;
+}
+
+/// Split a string input by [] pairs.
+fn generate_by_brackets(contents_vector: &String) -> Vec<String> {
+    let words: Vec<String> = Regex::new(r"\W+")
+        .unwrap()
+        .split(&contents_vector.to_owned())
+        .map(|x| x.to_string())
+        .collect();
+
+    return words
+}
+
+fn triage_commands(contents_vector: Vec<char>) {
+    enum CommandKind {
+        IncrementPointer,
+        DecrementPointer,
+        IncrementByte,
+        DecrementByte,
+        OutputByte,
+        AcceptByte,
+        SkipToAfterMatch,
+        SkipToBeforeMatch,
+        Comment,
+    }
+
+    for character in contents_vector {
+        match character {
+            '>' => {}
+            '<' => {}
+            '+' => {}
+            '-' => {}
+            '.' => {}
+            ',' => {}
+            '[' => {}
+            ']' => {}
+            _ => {},
+        }
+    }
 }
