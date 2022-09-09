4b15bdaf7817a9fea7aa6579017c66300d231324
medzernik <medzernik1@gmail.com>
2022-09-09 16:42:30 +0200

    file reading and character reading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
+
+use clap::Parser;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[clap(short, long, value_parser)]
+    file: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    let file_contents_char = load_file(args.file);
+}
+
+/// This function loads the file and populates an array of chars. Then it is returned and can be processed further.
+/// usage:
+/// ```let _ = loadfile("examples\01-hello-world.bf");```
+fn load_file(file_name: String) -> Vec<char> {
+    let mut file_contents_char: Vec<char> = Vec::new();
+
+    let file = File::open(file_name).expect("error opening the file");
+    let reader = BufReader::new(file);
+
+    //
+    for line in reader.lines() {
+        for ch in line.expect("Unable to read line").chars() {
+            file_contents_char.push(ch);
+        }
+    }
+
+    return file_contents_char;
 }
